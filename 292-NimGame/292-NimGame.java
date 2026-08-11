e452140fda59317151d950fa6dbc176661f93002
NITHIN-DT <nithindt6@gmail.com>
2026-08-11 21:21:40 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,10 @@
+// Last updated: 11/08/2026, 21:21:42
+class Solution {
+    public boolean canWinNim(int n) {
+      if(n%4==0)
+        return false;
+      else
+        return true;
+        
+    }
+}
