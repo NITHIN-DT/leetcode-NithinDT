9588c200f2a383932741b12e54f6fd1bc7c832a1
NITHIN-DT <nithindt6@gmail.com>
2026-08-11 21:21:57 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -0,0 +1,21 @@
+// Last updated: 11/08/2026, 21:21:59
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] count = new int[26]; // for lowercase English letters
+
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++;
+        }
+
+        for (char c : t.toCharArray()) {
+            count[c - 'a']--;
+            if (count[c - 'a'] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
