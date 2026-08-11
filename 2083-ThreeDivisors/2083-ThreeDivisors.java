54779508173e9d9ac3dc3faa59effcd5c6cd2b77
NITHIN-DT <nithindt6@gmail.com>
2026-08-11 21:19:19 +0530

    Create 2083-ThreeDivisors

---
+++ 2083-ThreeDivisors/2083-ThreeDivisors.java
@@ -0,0 +1,15 @@
+// Last updated: 11/08/2026, 21:19:21
+class Solution {
+    public boolean isThree(int n) {
+        int count = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                count++;
+            }
+        }
+        
+        return count == 3;
+
+    }
+}
